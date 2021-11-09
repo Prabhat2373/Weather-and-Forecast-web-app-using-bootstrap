68e32d8aa65b44b274c248c8966b4513ab713d65
prabhat2373 <prabhattambe10@gmail.com>
2021-11-09 16:11:04 +0530

    latest update

--- index.js
+++ index.js
@@ -9,63 +9,115 @@ let cityName = document.getElementById('cityName');
 let weatherBtn = document.getElementById('submitBtn');
 
 // Add Event Listener to WeatherBtn
-weatherBtn.addEventListener('click',weatherCity);
+weatherBtn.addEventListener('click', weatherCity);
 
 // Creating a function which is placed in the EventListener
-function weatherCity(){
-    if(cityName.value == ""){
+function weatherCity() {
+    if (cityName.value == "") {
         alert('Please Enter City Name to get weather')
+    }
+    else {
+        // console.log(cityName.value)
+        let apikey = '6b3bf6db669b4be582982724212510';
+        let cityValue = cityName.value;
+        // url = `http://api.weatherapi.com/v1/current.json?key=${apikey}=${cityName.value}&aqi=yes`
+        url = `http://api.weatherapi.com/v1/current.json?key=6b3bf6db669b4be582982724212510&q=${cityValue}&aqi=yes`
+
+        fetch(url, {
+            method: "GET",
+            headers: { 'content-type': 'application/json' }
+        }).then((res) => {
+            return res.json()
+        }).then((data) => {
+            // console.log(data)
+            let cityName = data.location.name
+            let currentTempC = data.current.temp_c
+            let currentTempF = data.current.temp_f
+            let currentFeel = data.current.feelslike_c
+            let condition = data.current.condition.text
+            let country = data.location.country
+
+            // Fetching data from localstorage
+            let Weather = localStorage.getItem("weather");
+            // Adding it validition
+            if (Weather == null) {
+                // Creating an empty Array 
+                weathObj = [];
+            }
+            else {
+                // parsing the localstorage data string to object
+                weathObj = JSON.parse(Weather);
+            }
+            // Creating an object of data
+            let myObj = {
+                name: cityName,
+                temp: currentTempC,
+                tempf: currentTempF,
+                condition: condition,
+                country: country,
+                // feel:currentFeel
+            }
+            // pushing myobj to weatherobj
+            weathObj.push(myObj);
+            // Setting data to lacalStorage in string format
+            localStorage.setItem("weather", JSON.stringify(weathObj));
+         
+        });
+        cityName.value = ""
+        // calling showWeather() function which is show data in ui
+       showWeather();
+
+        setTimeout(() => {
+            location.reload()
+        }, 1000);
+    };
+};
+// Creating a showWeather Function which get data from localstorage and show in ui
+function showWeather() {
+    let Weather = localStorage.getItem("weather");
+    if (Weather == null) {
+        weathObj = [];
+    }
+    else {
+        weathObj = JSON.parse(Weather);
+    }
+    let str = "";
+    // Adding ForEach Loop to weatherObj for fetching all the elements inside weatherObj
+    weathObj.forEach(function(element,index){
+        str += `
+                 <tr>
+                 <td>${element.name}</td>
+                 <td>${element.country}</td>
+                 <td>${element.temp}&deg;C/${element.tempf} F</td>
+                 <td>${element.condition}</td>
+                 <td><button class="btn btn-primary"  id="${index}" onclick="deleteWeather(this.id)">delete</button></td>
+               </tr>
+                 `
+        let tbody = document.getElementById('tbody');
+        tbody.innerHTML = str;
+        
+    });
+}
+// Creating a delete function to delete item
+function deleteWeather(index){
+    let Weather = localStorage.getItem("weather");
+    if (Weather == null) {
+        weathObj = [];
+    }
+    else {
+        weathObj = JSON.parse(Weather);
+    }
+    // adding splice method to weatherObj because its an array so we can add array methods on it
+    if(index == 0){
+        weathObj.splice(index,1);
+        location.reload()
     }
     else
     {
-    // console.log(cityName.value)
-    let apikey = '6b3bf6db669b4be582982724212510';
-    let cityValue = cityName.value;
-    // url = `http://api.weatherapi.com/v1/current.json?key=${apikey}=${cityName.value}&aqi=yes`
-    url = `http://api.weatherapi.com/v1/current.json?key=6b3bf6db669b4be582982724212510&q=${cityValue}&aqi=yes`
-    
-    // let response = await fetch(url);
-    // let res = await response.text();
-    // return res;
-    fetch(url,{
-        method:"GET",
-        headers:{'content-type':'application/json'}
-    }).then((res)=>{
-        return res.json()
-    }).then((data)=>{
-        // console.log(data)
-        // str = ""
-        let cityname = data.location.name
-        let currentTempC = data.current.temp_c
-        let currentTempF = data.current.temp_f
-        let currentFeel = data.current.feelslike_c
-        let condition = data.current.condition.text
-        let country = data.location.country
-
-        let myObj = {
-            name:cityName,
-            temp:currentTempC,
-            tempf:currentTempF,
-            condition:condition,
-            country:country
-        }
-        let localStg = localStorage.setItem("Weather",JSON.stringify(myObj));
-        // let localStg = localStorage.setItem("Weather",myObj);
-        // console.log(localStg)
-        // console.log(country)
-        let getstg = JSON.parse(localStorage.getItem('Weather'))
-        // console.log(getstg.condition)
-        let str = `
-        <tr>
-        <td>${cityname}</td>
-        <td>${country}</td>
-        <td>${currentTempC}&deg;C/${currentTempF} F</td>
-        <td>${getstg.condition}</td>
-        <td>${currentFeel}&deg;C</td>
-      </tr>
-        `
-        let tbody = document.getElementById('tbody');
-        tbody.innerHTML =str;
-    })
+        weathObj.splice(index,1);
+    }
+    localStorage.setItem("weather", JSON.stringify(weathObj));
+    // calling showWeather function
+    showWeather()  
 }
-}
+showWeather() // calling showWeather() function universally (Important)
